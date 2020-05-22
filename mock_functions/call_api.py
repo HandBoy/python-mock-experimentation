bd420590aca5970f76d6b3e69b6c6100b6bec94a
Handerson <handersonmedeiros@gmail.com>
2020-05-22 14:23:42 -0300

    feat: return details if error in request

--- mock_functions/call_api.py
+++ mock_functions/call_api.py
@@ -1,31 +1,26 @@
 import requests
-from requests import ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError
-
-
-'''
-ConnectionError 502
-HTTPError 
-
-'''
+from requests import Timeout, ConnectionError
 
 def call_the_api(url):
-    #url = 'http://httpbin.org/get'
-
     try:
         req = requests.get(url, timeout=5)
 
-        if 'errors' in req.json():
-            return {'request_error': 'api_error_response'}
+        if not req.ok:
+            response = req.json()
+            if "detail" in response:
+                return {"error": response["detail"]}
+                
+            return {"request_error": "api_error_response"}
 
         return req.json()[0]
 
     except ConnectionError:
-        return {'request_error': 'ConnectionTimeout'}
+        return {"request_error": "ConnectionTimeout"}
     except Timeout:
-        return {'request_error': 'Timeout'}
+        return {"request_error": "Timeout"}
     except Exception as ex:
-        return {'request_error': ex}
+        return {"request_error": ex}
 
 
 if __name__ == "__main__":
-    call_the_api()
+    call_the_api("http://jsonplaceholder.typicode.com/todos")
